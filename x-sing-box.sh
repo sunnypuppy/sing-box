9f4210ac56a3841845e3b97ed3c99fca36966c62
sunnypuppy <wangyuanyuan1024@gmail.com>
2025-06-11 15:52:48 +0800

    update

--- x-sing-box.sh
+++ x-sing-box.sh
@@ -1084,7 +1084,7 @@ EOF
 
 main() {
     parse_parameters "$@" || exit 1
-    check_and_install_deps curl lsof pgrep openssl jq || exit 1
+    check_and_install_deps curl pgrep openssl jq || exit 1
     get_system_info --silent
 
     case "$main_action" in
